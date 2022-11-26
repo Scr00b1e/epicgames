75b5b910e1ea153d6966bfcc2d75d76059b2cbee
sa1kunX <samiddinsapirov@gmail.com>
2022-11-26 18:20:31 +0600

    updated components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import Header from './components/header/Header'
 import './global.scss'
+import Search from "./components/search/Search";
+import Card from "./components/card/Card";
 
 function App() {
   return (
     <div className="App">
-
+      <Header/>
+      <Search/>
+      <Card/>
     </div>
   );
 }
