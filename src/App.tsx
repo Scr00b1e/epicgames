b2481430403db19f558d0a42b82a74fdd45da683
Scr00b1e <kbolot312@gmail.com>
2022-11-27 13:23:01 +0600

    managed test1 branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+// import React from 'react';
 import './global.scss'
 import Search from "./components/search/Search";
 import Card from "./components/card/Card";
+import Header from './components/header/Header';
+import Intro from './components/Intro/Intro';
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
+      <Search />
+      <Intro />
+      <Card />
     </div>
   );
 }
--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -2,105 +2,105 @@ import React from 'react'
 import './card.scss'
 
 const Card: React.FC = () => {
-        return (
-            <section className="sec_1">
-                <div className="sec1_top">
-                    <h5>大特卖聚焦 </h5>
+    return (
+        <section className="sec_1">
+            <div className="sec1_top">
+                <h5>Sale </h5>
+            </div>
+            <div className="sec1_carousel">
+                {/*<button>*/}
+                {/*    <*/}
+                {/*</button>*/}
+                {/*<button>></button>*/}
+            </div>
+            <div className="sec1_bottom">
+                <div className="sec1_card">
+                    <div className="card_top">
+                        <img src="img/dracon.png" alt="" />
+                        <h4>Tiny Tina's Wonderlands</h4>
+                    </div>
+                    <div className="card_bottom">
+                        <p>-20%</p>
+                        <ul>
+                            <li>̶¥̶1̶9̶9̶</li>
+                            <li>¥159.20</li>
+                        </ul>
+                    </div>
+
                 </div>
-                <div className="sec1_carousel">
-                    {/*<button>*/}
-                    {/*    <*/}
-                    {/*</button>*/}
-                    {/*<button>></button>*/}
+                <div className="sec1_card2">
+                    <div className="card_top">
+                        <img src="img/sifu.png" alt="" />
+                        <h4>Sifu</h4>
+                    </div>
+                    <div className="card_bottom">
+                        <p>-10%</p>
+                        <ul>
+                            <li>̶¥̶1̶3̶9̶̶</li>
+                            <li>¥125.10</li>
+                        </ul>
+                    </div>
+
                 </div>
-                <div className="sec1_bottom">
-                    <div className="sec1_card">
-                        <div className="card_top">
-                            <img src="img/dracon.png" alt=""/>
-                                <h4>Tiny Tina's Wonderlands</h4>
-                        </div>
-                        <div className="card_bottom">
-                            <p>-20%</p>
-                            <ul>
-                                <li>̶¥̶1̶9̶9̶</li>
-                                <li>¥159.20</li>
-                            </ul>
-                        </div>
-
+                <div className="sec1_card3">
+                    <div className="card_top">
+                        <img src="img/final.png" alt="" />
+                        <h4>FINAL FANTASY VII REMAKE...</h4>
                     </div>
-                    <div className="sec1_card2">
-                        <div className="card_top">
-                            <img src="img/sifu.png" alt=""/>
-                                <h4>Sifu</h4>
-                        </div>
-                        <div className="card_bottom">
-                            <p>-10%</p>
-                            <ul>
-                                <li>̶¥̶1̶3̶9̶̶</li>
-                                <li>¥125.10</li>
-                            </ul>
-                        </div>
-
+                    <div className="card_bottom">
+                        <p>-20%</p>
+                        <ul>
+                            <li>̶¥̶1̶9̶9̶</li>
+                            <li>¥159.20</li>
+                        </ul>
                     </div>
-                    <div className="sec1_card3">
-                        <div className="card_top">
-                            <img src="img/final.png" alt=""/>
-                                <h4>FINAL FANTASY VII REMAKE...</h4>
-                        </div>
-                        <div className="card_bottom">
-                            <p>-20%</p>
-                            <ul>
-                                <li>̶¥̶1̶9̶9̶</li>
-                                <li>¥159.20</li>
-                            </ul>
-                        </div>
 
-                    </div>
-                    <div className="sec1_card4">
-                        <div className="card_top">
-                            <img src="img/rdr2.png" alt=""/>
-                                <h4>Red Dead Redemption 2</h4>
-                        </div>
-                        <div className="card_bottom">
-                            <p>-50%</p>
-                            <ul>
-                                <li>̶¥̶2̶2̶8̶̶</li>
-                                <li>¥114</li>
-                            </ul>
-                        </div>
-
-                    </div>
-                    <div className="sec1_card5">
-                        <div className="card_top">
-                            <img src="img/cyberpunk.png" alt=""/>
-                                <h4>Cyberpunk 2077</h4>
-                        </div>
-                        <div className="card_bottom">
-                            <p>-50%</p>
-                            <ul>
-                                <li>̶¥̶2̶8̶0̶̶</li>
-                                <li>¥140</li>
-                            </ul>
-                        </div>
-
-                    </div>
-                    <div className="sec1_card6">
-                        <div className="card_top">
-                            <img src="img/kena.png" alt=""/>
-                                <h4>Kena: Bridge of Spirits</h4>
-                        </div>
-                        <div className="card_bottom">
-                            <p>-35%</p>
-                            <ul>
-                                <li>̶̶¥̶1̶3̶7̶</li>
-                                <li>¥89.05</li>
-                            </ul>
-                        </div>
-
-                    </div>
                 </div>
-            </section>
-        )
-    }
+                <div className="sec1_card4">
+                    <div className="card_top">
+                        <img src="img/rdr2.png" alt="" />
+                        <h4>Red Dead Redemption 2</h4>
+                    </div>
+                    <div className="card_bottom">
+                        <p>-50%</p>
+                        <ul>
+                            <li>̶¥̶2̶2̶8̶̶</li>
+                            <li>¥114</li>
+                        </ul>
+                    </div>
+
+                </div>
+                <div className="sec1_card5">
+                    <div className="card_top">
+                        <img src="img/cyberpunk.png" alt="" />
+                        <h4>Cyberpunk 2077</h4>
+                    </div>
+                    <div className="card_bottom">
+                        <p>-50%</p>
+                        <ul>
+                            <li>̶¥̶2̶8̶0̶̶</li>
+                            <li>¥140</li>
+                        </ul>
+                    </div>
+
+                </div>
+                <div className="sec1_card6">
+                    <div className="card_top">
+                        <img src="img/kena.png" alt="" />
+                        <h4>Kena: Bridge of Spirits</h4>
+                    </div>
+                    <div className="card_bottom">
+                        <p>-35%</p>
+                        <ul>
+                            <li>̶̶¥̶1̶3̶7̶</li>
+                            <li>¥89.05</li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+    )
+}
 
 export default Card
