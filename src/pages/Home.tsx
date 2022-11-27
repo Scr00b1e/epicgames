47fdf793fcf4b19076663df8fa9f16580ef93602
Scr00b1e <kbolot312@gmail.com>
2022-11-27 22:40:10 +0600

    added browse component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import Search from "../components/search/Search";
 import Card from "../components/card/Card";
 import Intro from '../components/Intro/Intro';
 import FreeGames from '../components/freeGames/FreeGames';
+import Browse from '../components/browse/Browse';
 
 const Home: React.FC = () => {
     return (
@@ -11,6 +12,7 @@ const Home: React.FC = () => {
             <Intro />
             <Card />
             <FreeGames />
+            <Browse />
         </div>
     )
 }
