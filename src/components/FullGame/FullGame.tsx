8c49caa322a0e492e380c0a6a0a5c39f1f6c4098
sa1kunX <samiddinsapirov@gmail.com>
2022-12-04 20:08:12 +0600

    change

--- src/components/FullGame/FullGame.tsx
+++ src/components/FullGame/FullGame.tsx
@@ -34,14 +34,49 @@ const FullGame: React.FC = () => {
     }
 
     return (
-        <div className='container'>
-            <div className="fullGame">
-                FullGame
-                <h1>{game.title}</h1>
-                <img src={game.image} alt="" />
-                <p>{game.price}</p>
+    <div className={'spider'}>
+        <div className="container">
+
+            <div className="spider_left">
+
+                <div className="left_top">
+
+                    <h1>MARVEL Spider-Man: Miles Morales</h1>
+
+                    <div className="raiting">
+                        <div className="stars">
+                            <img src="./img/stars.png" alt=""/>
+                            <h3>4.8</h3>
+                        </div>
+                        <p>great boss battles</p>
+                        <h5>diverse cast of characters</h5>
+                    </div>
+
+                    <div className="miles_img">
+                        <img src="./img/milesmorales.webp" alt=""/>
+                    </div>
+
+                    <div className="trailer">
+                        <img src="./img/milesmorales.webp" alt=""/>
+                        <img src="./img/milesmorales.webp" alt=""/>
+                        <img src="./img/milesmorales.webp" alt=""/>
+                        <img src="./img/milesmorales.webp" alt=""/>
+                    </div>
+
+                </div>
+
+                <div className="left_bottom">
+
+                </div>
+
+            </div>
+
+            <div className="spider_right">
+                <div className="right_top"></div>
+                <div className="right_bottom"></div>
             </div>
         </div>
+    </div>
     )
 }
 
