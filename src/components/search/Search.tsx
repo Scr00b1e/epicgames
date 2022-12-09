22cb409c3649416774e1811faa230516465681ee
Scr00b1e <kbolot312@gmail.com>
2022-12-09 20:57:52 +0600

    search updated

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
- import './search.scss'
+import './search.scss'
 
 const Search: React.FC = () => {
     return (
-        <div className="epicgames_search">
-            <input placeholder="search" type="search"/>
-                <img src="./img/search.png" alt=""/>
-                    <ul>
-                        <li>Explore</li>
-                        <li>Browse</li>
-                        <li>News</li>
-                    </ul>
+        <div className="container">
+            <div className="navigate">
+                <div className="navigate__search">
+                    <img src="./img/search.png" alt="" />
+                    <input placeholder="search" type="search" />
+                </div>
+                <ul>
+                    <li className='active'>Explore</li>
+                    <li>Browse</li>
+                    <li>News</li>
+                </ul>
+            </div>
         </div>
     )
 }
