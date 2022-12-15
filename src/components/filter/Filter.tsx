9b2acc0a9e7f776248daca2ffff8ad5e2599dd93
Scr00b1e <kbolot312@gmail.com>
2022-12-15 22:33:04 +0600

    preparing

--- src/components/filter/Filter.tsx
+++ src/components/filter/Filter.tsx
@@ -4,7 +4,7 @@ import './filter.scss'
 const Filter: React.FC = () => {
     const [active, setActive] = React.useState(null)
     const onClickFilter = (index: any) => {
-        if(active === index) {
+        if (active === index) {
             return setActive(null)
         }
 
@@ -17,96 +17,48 @@ const Filter: React.FC = () => {
             id: 0
         },
         {
-            name: 'Price',
+            name: 'Types',
             id: 1
         },
         {
-            name: 'platform',
+            name: 'Features',
             id: 2
         }
     ]
 
-  return (
-    <aside className='filter'>
-        <div className="filter__words">
-            <h1 className='filter__title'>Filter</h1>
-            <h1 className='filter__title'>Reset</h1>
-        </div>
-        <div className="filter__search">
+    return (
+        <aside className='filter'>
+            <div className="filter__words">
+                <h1 className='filter__title'>Filter</h1>
+                <h1 className='filter__title'>Reset</h1>
+            </div>
+            <div className="filter__search">
                 <img src="./img/search.png" alt="" />
                 <input placeholder="search" type="search" />
-        </div>
-        <ul className='filter__list'>
-            {/* <br/>
-            <hr/>
-            <li className='filter__item' onClick={() => onClickFilter(0)} >
-                <div className='filter__visible'>
-                    <p className="filter__text">
-                        Genre
-                    </p>
-                    <img src="./img/more.svg" alt="" />
-                </div>
-                <ul className={active === 0 ? 'active' : ''}>
-                    <li>
-                        test
-                    </li>
-                </ul>
-            </li>
-            <hr/>
-            <br/>
-            <hr/>
-            <li className='filter__item' onClick={() => onClickFilter(1)}>
-                <div className='filter__visible'>
-                    <p className="filter__text">
-                        price
-                    </p>
-                    <img src="./img/more.svg" alt="" />
-                </div>
-                <ul className={active === 1 ? 'active' : ''}>
-                    <li>
-                        test
-                    </li>
-                </ul>
-            </li>
-            <hr/>
-            <br/>
-            <hr/>
-            <li className='filter__item' onClick={() => onClickFilter(2)}>
-                <div className='filter__visible'>
-                    <p className="filter__text">
-                        platform
-                    </p>
-                    <img src="./img/more.svg" alt="" />
-                </div>
-                <ul className={active === 2 ? 'active' : ''}>
-                    <li>
-                        test
-                    </li>
-                </ul>
-            </li>
-            <hr/> */}
-            {
-                filter.map((obj, index) => (
-                    <li className='filter__item' key={obj.id} onClick={() => onClickFilter(index)}>
-                        <div className='filter__visible'>
-                            <p className="filter__text">
-                                {obj.name}
-                            </p>
-                            <img src="./img/more.svg" alt="" />
-                        </div>
-                        <ul className={active === index ? 'active' : ''}>
-                            <li>
-                                test
-                            </li>
-                        </ul>
-                    </li>
-                ))
-            }
+            </div>
+            <ul className='filter__list'>
+                {
+                    filter.map((obj, index) => (
+                        <li className='filter__item' key={obj.id} onClick={() => onClickFilter(index)}>
+                            <div className='filter__visible'>
+                                <p className="filter__text">
+                                    {obj.name}
+                                </p>
+                                <img src="./img/more.svg" alt="" />
+                            </div>
+                            <ul className={active === index ? 'active' : ''}>
+                                <li>
+                                    test
+                                </li>
+                            </ul>
+                        </li>
+                    ))
+                }
 
-        </ul>
-        
-    </aside>
-  )
+            </ul>
+
+        </aside>
+    )
 }
 
 export default Filter
--- src/components/sort/Sort.tsx
+++ src/components/sort/Sort.tsx
@@ -4,10 +4,10 @@ import './sort.scss'
 const Sort: React.FC = () => {
   return (
     <select className='sort'>
-        <option value="Popular" selected>Popular</option>
-        <option value="Alphabet">Alphaber</option>
-        <option value="pricelow">Price: Low</option>
-        <option value="pricehigh">Price: high</option>
+      <option value="Popular" selected>Popular</option>
+      <option value="Alphabet">Alphabetic</option>
+      <option value="pricelow">Price: Low</option>
+      <option value="pricehigh">Price: high</option>
     </select>
   )
 }
