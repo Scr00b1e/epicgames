81da667d0794c23d340050dfaf88372d865e1c88
Scr00b1e <kbolot312@gmail.com>
2022-12-19 23:18:08 +0600

    working on redux

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -9,11 +9,12 @@ type PropsType = {
     id: string
 }
 
-const Card: React.FC<PropsType> = ({title, image, price, noPrice}) => {
+const Card: React.FC<PropsType> = ({ title, image, price, noPrice }) => {
     return (
         <div className="card">
             <div className="card__top">
-                <img src={image} alt="" />
+                <img src="./img/add.png" alt="" className='card__add' />
+                <img src={image} alt="" className='card__img' />
                 <h4>{title}</h4>
             </div>
             <div className="card__bottom">
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,14 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './header.scss'
 
-// https://store.epicgames.com/
 
 const Header: React.FC = () => {
     return (
         <div className='header'>
             <div className="header_left">
                 <Link to={'/'}>
-                    <img src="./img/icon.png" alt="" className='header__img'/>
+                    <img src="./img/icon.png" alt="" className='header__img' />
                 </Link>
                 <ul>
                     <li>Store</li>
@@ -18,16 +17,9 @@ const Header: React.FC = () => {
                     </a>
                 </ul>
             </div>
-            <a href="https://store.epicgames.com/">
-                <div className="header_right">
-                    <img src="./img/Union.png" alt="" />
-                    <div className='header__login'>
-                        <img src="./img/Vector.png" alt="" />
-                        <p>Log in</p>
-                    </div>
-                    <button>Get client</button>
-                </div>
-            </a>
+            <div className="header_right">
+                <h1 className='header__title'>Wishlist</h1>
+            </div>
         </div>
     )
 }
