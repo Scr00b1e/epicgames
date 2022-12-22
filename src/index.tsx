05a8bfbb65c2c0e28ad7eabf4de1435d2b76cc24
Scr00b1e <kbolot312@gmail.com>
2022-12-22 18:27:46 +0600

    managed redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { store } from './redux/store';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -10,7 +12,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/redux/slices/wishSlice/wishSlice.ts
@@ -0,0 +1,61 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+export type WishType = {
+    id: number
+    title: string
+    price: number
+    image: string
+    count: number
+}
+
+interface WishInitialState {
+    payload: number
+    subjects: WishType[]
+}
+
+const initialState: WishInitialState = {
+    payload: 0,
+    subjects: [],
+}
+
+export const wishSlice = createSlice({
+    name: 'wish',
+    initialState,
+    reducers: {
+        addItem(state: WishInitialState, action: PayloadAction<WishType>) {
+            const findItem = state.subjects.find((obj) => obj.id === action.payload.id)
+
+            if (findItem) {
+                findItem.count += 1;
+            } else {
+                state.subjects.push({
+                    ...action.payload,
+                    count: 1,
+                });
+            }
+        },
+        minusItem(state: WishInitialState, action: PayloadAction<WishType>) {
+            const findItem = state.subjects.find((obj) => obj.id === action.payload.id)
+
+            if (findItem) {
+                findItem.count -= 1;
+            } else {
+                state.subjects.push({
+                    ...action.payload,
+                    count: 1,
+                });
+            }
+
+        },
+        removeItem(state: WishInitialState, action: PayloadAction<number>) {
+            state.subjects = state.subjects.filter((obj) => obj.id !== action.payload)
+        },
+        clearItem(state) {
+            state.subjects = []
+        }
+    },
+})
+
+export const { addItem, minusItem, removeItem } = wishSlice.actions
+
+export default wishSlice.reducer
